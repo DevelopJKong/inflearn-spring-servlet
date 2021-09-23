6fb6b268933e500fd0cda60faa38ed56a841d6f3
DevelopJKong <ybaram40@gmail.com>
2021-09-23 12:41:36 +0900

    뷰 리졸버 완료

--- src/main/java/hello/servelt/ServeltApplication.java
+++ src/main/java/hello/servelt/ServeltApplication.java
@@ -3,6 +3,9 @@ package hello.servelt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 
 @ServletComponentScan //서블릿 자동 등록
@@ -12,4 +15,8 @@ public class ServeltApplication {
 		SpringApplication.run(ServeltApplication.class, args);
 	}
 
+//	@Bean
+//	ViewResolver internalResourceViewResolve(){
+//		return new InternalResourceViewResolver("/WEB-INF/views/",".jsp");
+//	}
 }
